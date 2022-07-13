3946b44a49dcd8f4d88b92b001d1bb2b2178f68e
Matthias Müller <matthias.mueller@trivadis.com>
2022-07-13 10:25:43 +0200

    WIP

--- Sources/Application/Web/Areas/HealthCheck/HealthCheckController.cs
+++ Sources/Application/Web/Areas/HealthCheck/HealthCheckController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mmu.DrMuellersExampleApp.Web.Areas.HealthCheck
 {
+    [AllowAnonymous]
     [Route("")]
     [ApiController]
     public class HealthCheckController : ControllerBase

e660ca0ff9a9bc0b9b443eb595ee729a13fe12be
Matthias Müller <matthias.mueller@trivadis.com>
2022-07-14 14:55:43 +0200

    WIP

--- Sources/Application/Web/Infrastructure/Initialization/AppInitialization.cs
+++ Sources/Application/Web/Infrastructure/Initialization/AppInitialization.cs
@@ -13,7 +13,7 @@ namespace Mmu.DrMuellersExampleApp.Web.Infrastructure.Initialization
             var basePath = configuration.GetSection($"{AppSettings.SectionKey}:{nameof(AppSettings.AppBasePath)}").Get<string>();
             app.UsePathBase(basePath);
 
-            Console.WriteLine($"Using basePath {basePath}")
+            Console.WriteLine($"Using basePath {basePath}");
 
             app.UseMiddleware<ConsoleOutputMiddleware>();
             app.UseGlobalExceptionHandler();

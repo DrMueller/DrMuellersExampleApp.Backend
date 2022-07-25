fbd6d5ef0090649e45b976d65f9dcbe1cae0698c
Matthias Müller <matthias.mueller@trivadis.com>
2022-07-25 16:10:25 +0200

    WIP

--- Sources/Application/CrossCutting/Services/Settings/Provisioning/Models/AppSettings.cs
+++ Sources/Application/CrossCutting/Services/Settings/Provisioning/Models/AppSettings.cs
@@ -8,6 +8,7 @@ namespace Mmu.DrMuellersExampleApp.CrossCutting.Services.Settings.Provisioning.M
         public const string SectionKey = "AppSettings";
         public string SecretKey { get; set; } = null!;
         public string AppBasePath { get; set; } = null!;
-        public SecuritySettings SecuritySettings { get; set; } = null!;
+        public string ApiPassword { get; set; } = null!;
+        public string ApiUserName { get; set; } = null!;
     }
 }

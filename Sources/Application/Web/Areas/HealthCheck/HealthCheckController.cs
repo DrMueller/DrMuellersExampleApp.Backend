8c9e3f11fb8cc07f339d7755dbfd81a53fc6be51
Matthias Mueller U810688 <Matthias.Mueller@mobi.ch>
2022-09-29 21:53:57 +0200

    WIP

--- Sources/Application/Web/Areas/HealthCheck/HealthCheckController.cs
+++ Sources/Application/Web/Areas/HealthCheck/HealthCheckController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Mmu.DrMuellersExampleApp.CrossCutting.Services.Settings.Provisioning.Models;
+using Mmu.DrMuellersExampleApp.CrossCutting.Services.Settings.Provisioning.Services;
 
 namespace Mmu.DrMuellersExampleApp.Web.Areas.HealthCheck;
 
@@ -8,9 +10,22 @@ namespace Mmu.DrMuellersExampleApp.Web.Areas.HealthCheck;
 [ApiController]
 public class HealthCheckController : ControllerBase
 {
+    private readonly IAppSettingsProvider _appsSettingsProvider;
+
+    public HealthCheckController(IAppSettingsProvider appsSettingsProvider)
+    {
+        _appsSettingsProvider = appsSettingsProvider;
+    }
+
     [HttpGet]
     public IActionResult GetCheck()
     {
         return Ok();
+    }
+
+    [HttpGet("settings")]
+    public ActionResult<AppSettings> GetAppSettings()
+    {
+        return Ok(_appsSettingsProvider.Settings);
     }
 }

8b77093dda79d65b776a5d46ced45080e8b0f9b7
Matthias Mueller U810688 <Matthias.Mueller@mobi.ch>
2022-09-29 22:28:58 +0200

    WIP

--- Sources/Application/CrossCutting/Services/Settings/Provisioning/Models/AppSettings.cs
+++ Sources/Application/CrossCutting/Services/Settings/Provisioning/Models/AppSettings.cs
@@ -6,10 +6,7 @@ namespace Mmu.DrMuellersExampleApp.CrossCutting.Services.Settings.Provisioning.M
 public class AppSettings
 {
     public const string SectionKey = "AppSettings";
-    public string ApiPassword { get; set; } = null!;
-    public string ApiUserName { get; set; } = null!;
     public string AppBasePath { get; set; } = null!;
-    public string SecretKey { get; set; } = null!;
     public string AppVersion { get; set; } = null!;
 
     public AzureAdSettings AzureAd { get; set; } = null!;

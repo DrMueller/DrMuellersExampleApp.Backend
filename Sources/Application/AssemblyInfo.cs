d626a06166b0c49bc01133a8d5ab11bc83fda18b
Müller Matthias <matthias.mueller@noser.com>
2023-01-18 07:33:17 +0100

    WIP

---
+++ Sources/Application/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mmu.DrMuellersExampleApp")]
+[assembly: AssemblyDescription("Mmu.DrMuellersExampleApp")]
+[assembly: AssemblyCompany("DrMmeller")]
+[assembly: AssemblyProduct("Template")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("")]
+[assembly: AssemblyInformationalVersion("")]
